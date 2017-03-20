1de53d6dba18e8801b1005d345bbcd3c302546e8
Damian Matusek <shmatuza@hotmail.com>
2017-03-20 22:20:43 +0100

    Key Down procedure

--- KalkulatorWPF/MainWindow.xaml.cs
+++ KalkulatorWPF/MainWindow.xaml.cs
@@ -20,9 +20,131 @@ namespace KalkulatorWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double number;
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void NumberButton_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.NumPad0:
+                    {
+                        zeroButton_Click(sender, e);
+                        break;
+                    }
+                case Key.NumPad1:
+                    {
+                        oneButton_Click(sender, e);
+                        break;
+                    }
+                case Key.NumPad2:
+                    {
+                        twoButton_Click(sender, e);
+                        break;
+                    }
+                case Key.NumPad3:
+                    {
+                        threeButton_Click(sender, e);
+                        break;
+                    }
+                case Key.NumPad4:
+                    {
+                        fourButton_Click(sender, e);
+                        break;
+                    }
+                case Key.NumPad5:
+                    {
+                        fiveButton_Click(sender, e);
+                        break;
+                    }
+                case Key.NumPad6:
+                    {
+                        sixButton_Click(sender, e);
+                        break;
+                    }
+                case Key.NumPad7:
+                    {
+                        sevenButton_Click(sender, e);
+                        break;
+                    }
+                case Key.NumPad8:
+                    {
+                        eightButton_Click(sender, e);
+                        break;
+                    }
+                case Key.NumPad9:
+                    {
+                        nineButton_Click(sender, e);
+                        break;
+                    }
+            }
+        }
+
+        private void zeroButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Display.Text == "" || Display.Text.Contains(',') || !String.IsNullOrEmpty(Display.Text) && Display.Text[0] != '0')
+            {
+                Display.Text += "0";
+                number = double.Parse(Display.Text);
+
+            }
+        }
+
+        private void oneButton_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text += "1";
+            number = double.Parse(Display.Text);
+        }
+
+        private void twoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text += "2";
+            number = double.Parse(Display.Text);
+        }
+
+        private void threeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text += "3";
+            number = double.Parse(Display.Text);
+        }
+
+        private void fourButton_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text += "4";
+            number = double.Parse(Display.Text);
+        }
+
+        private void fiveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text += "5";
+            number = double.Parse(Display.Text);
+        }
+
+        private void sixButton_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text += "6";
+            number = double.Parse(Display.Text);
+        }
+
+        private void sevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text += "7";
+            number = double.Parse(Display.Text);
+        }
+
+        private void eightButton_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text += "8";
+            number = double.Parse(Display.Text);
+        }
+
+        private void nineButton_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text += "9";
+            number = double.Parse(Display.Text);
         }
     }
 }
